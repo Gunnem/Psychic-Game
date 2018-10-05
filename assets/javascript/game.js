92db1f1b68a00008e74f9ee5c3dadce08363670b
Matthew Kim <matthewkim@Matthews-iMac.local>
2018-10-04 22:11:33 -0700

    final rev

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,31 +3,43 @@ var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 var win = 0;
 var lose = 0;
 var guessesLeft = 9;
-var yourGuesses = 0;
+var yourGuesses = [];
+var computerPick = [];
+
+window.onload = function() {
+    var compPick = options[Math.floor(Math.random()*options.length)];
+    computerPick.push(compPick);
+    console.log(computerPick[0]);
+}
 
 document.onkeyup = function() {
-    var psychicChooses = options[Math.floor(Math.random()*options.length)];
-    console.log(psychicChooses);
-    var userGuess = String.fromCharCode(event.keyCode).
+        var userGuess = String.fromCharCode(event.keyCode).
         toLowerCase();
+        yourGuesses.push(userGuess);
 
-    if((userGuess === psychicChooses) && (guessesLeft > 0)){
+    if((userGuess === computerPick[0]) && (guessesLeft > 0)){
         win++;
         guessesLeft = 9;
-        yourGuesses = 0;
+        yourGuesses.length = 0;
+        computerPick.length = 0;
+        var compPick = options[Math.floor(Math.random()*options.length)];
+        computerPick.push(compPick);
+        console.log(computerPick[0]);
         alert("You are psychic!");
         
-    } else if((userGuess !== psychicChooses) && (guessesLeft > 0)){
+    } else if((userGuess !== computerPick[0]) && (guessesLeft > 0)){
         guessesLeft--;
-        yourGuesses++;
-        
+            
     } else{
         win = 0;
         lose++;
         guessesLeft = 9;
-        yourGuesses = 0;
+        yourGuesses.length = 0;
+        var compPick = options[Math.floor(Math.random()*options.length)];
+        computerPick.push(compPick);
+        console.log(computerPick[0]);
     }
-       
+   
     var html = "<p><h1>The Psychic Game</h1></p>" +
     "<p>Type the letter I'm thinking of...</p>" +
     "<p>Wins: " + win + "</p>" +
